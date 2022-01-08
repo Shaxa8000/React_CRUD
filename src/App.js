b6a642334d5dae292aac6e28105568b4c46c124c
Shaxa8000 <shaxboz.yaxyoyev@bk.ru>
2022-01-08 16:04:46 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,121 +1,105 @@
-import React from 'react';
-import { data } from './data';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      status: '',
-      active: null,
-      data: {
-        status: 'student',
-        datalist: data,
+const App = () => {
+  const [data, setData] = useState([
+    { id: 1, name: 'Eshmat', status: 'Student' },
+    { id: 2, name: 'Toshmat', status: 'Mentor' },
+    { id: 3, name: 'Gulmat', status: 'Developer' },
+    { id: 4, name: 'Gulbashakar', status: 'Tester' },
+  ]);
+
+  const [name, setName] = useState('');
+  const [status, setStatus] = useState('');
+  const [select, setSelect] = useState(null);
+  const [title, setTitle] = useState('');
+
+  //Delete
+
+  const onDelete = (id) => {
+    var res = data.filter((value) => value.id !== id);
+    setData(res);
+  };
+
+  //Create (add)
+
+  const onSave = () => {
+    const newData = [
+      ...data,
+      {
+        id: data.length + 1,
+        name: name,
+        status: status,
       },
-    };
+    ];
+
+    setData(newData);
+  };
+
+  //Update
+
+  const getEdit = (value) => {
+    setSelect(value.id);
+    setTitle(value.name);
+    setStatus(value.status);
+  };
+
+
+  const onEditSave = () => {
+    const newArray = data.map((value) => select == value.id ? { ...value, name: title, status: status } : value);
+
+    setData(newArray);
+    setSelect(null)
   }
-  render() {
-    // onDelete
 
-    const onDelete = (id) => {
-      let res = this.state.data.datalist.filter(value => value.id !== id)
 
-      this.setState({
-        data: {
-          ...this.state.data,
-          datalist: res,
-        },
-      });
-    };
 
-    // addUser
-
-    const onChange = (e) => {
-      console.log(e)
-      this.setState({
-        [e.target.name]: e.target.value,
-      })
-    };
-
-    // onSave button
-
-    const onSave = () => {
-      const newData = {
-        id: Date.now(),
-        name: this.state.name,
-        status: this.state.status,
-      };
-      this.setState({
-        data: {
-          ...this.state.data,
-          datalist: [...this.state.data.datalist, newData]
-        }
-      })
-    };
-
-    // onEdit
-
-    const onEdit = (id) => {
-      this.setState({
-        active: id
-      })
-    }
-
-    
-
-    return (
-      <div>
-        <div className='inputSection'>
-          <input
-            onChange={onChange}
-            type='text'
-            placeholder='enter your name'
-            name='name'
-          />
-          <input
-            onChange={onChange}
-            type='text'
-            placeholder='enter your status'
-            name='status'
-          />{' '}
-          <button className='btn' onClick={onSave}>
-            Save
-          </button>
-        </div>
-        <table border='1' width='100%'>
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Name</th>
-              <th>Status</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.data.datalist.map(({ id, name, status }) => (
-              <tr key={id}>
+  return (
+    <div>
+      <input
+        onChange={(e) => setName(e.target.value)}
+        type='text'
+        placeholder='enter your name'
+      />
+      <input
+        onChange={(e) => setStatus(e.target.value)}
+        type='text'
+        placeholder='enter your status'
+      />
+      <button onClick={onSave}>Add</button>
+      <table border='1' width='80%'>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Status</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((value) => {
+            return (
+              <tr key={value.id}>
+                <td>{value.id}</td>
+                <td>{select == value.id ? <input onChange={(e)=> setTitle(e.target.value)} type="text" value={title}/> : value.name}</td>
+                <td>{select == value.id ? <input onChange={(e) => setStatus(e.target.value)} type="text" value={status}/> : value.status}</td>
                 <td>
-                  {this.state.active === id ? <input name ='name' type ='text'onChange={onChange}/> : id}
-                </td>
-                <td>{name}</td>
-                <td>{status}</td>
-                <td>
-                  <button className='btn' onClick={() => onDelete(id)}>
-                    delete
-                  </button>
-                  <button className='btn' onClick={() => onEdit(id)}>
-                    {/* Conditional rendering */}
-                    {this.state.active === id  ? 'save' : 'edit'}
-                  </button>
+                  <button onClick={() => onDelete(value.id)}>delete</button>
+
+                  {select == value.id
+                    ? 
+                    <button onClick={onEditSave}>save</button>
+                   : 
+                    <button onClick={() => getEdit(value)}>edit</button>
+                  }
                 </td>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default App;
--- src/data.js
+++
@@ -1,9 +0,0 @@
-export const data = [
-  { id: 1, name: 'Eshmat', status: 'Student', email: 'john@gmail.com' },
-  { id: 2, name: 'Toshmat', status: 'Mentor', email: 'john@gmail.com' },
-  { id: 3, name: 'Gulmat', status: 'Student', email: 'john@gmail.com' },
-  { id: 4, name: 'Nurmat', status: 'Mentor', email: 'john@gmail.com' },
-  { id: 5, name: 'Eshmat', status: 'Student', email: 'john@gmail.com' },
-  { id: 6, name: 'Pirmat', status: 'Student', email: 'john@gmail.com' },
-  { id: 7, name: 'Hikmat', status: 'Student', email: 'john@gmail.com' },
-];
